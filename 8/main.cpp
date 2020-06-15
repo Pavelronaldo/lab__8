3e8a57f64aa86e95fc216c9294d11e6fa263836b
Pavel <pavel.m.3107@gmail.com>
2020-06-15 11:46:05 +0300

    Initial Commit

---
+++ 8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  8
+//
+//  Created by Pavel Ronaldo on 6/15/20.
+//  Copyright © 2020 Pavel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
